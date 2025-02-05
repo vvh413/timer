30eb3577eadc3f473930cc4c60b52ac00c8bac76
vvh <ns@ywa.red>
2025-02-05 04:02:30 +0300

    chore: minor refactoring + updated deps

--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,118 @@
-use std::process::exit;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
 
-use anyhow::Result;
 use clap::Parser;
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use eyre::Result;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::sync::Mutex;
 use tokio::task::JoinHandle;
 
-const TIMER_STEP_MS: u64 = 500;
-const TOGGLE_CHAR: char = '\n';
+const TIMER_STEP: Duration = Duration::from_millis(500);
+const TOGGLE_CHAR: u8 = b'\n';
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-  /// Hours
-  #[arg(short = 'H', long, default_value_t = 0)]
-  hours: u64,
+    /// Hours
+    #[arg(short = 'H', long, default_value_t = 0)]
+    hours: u64,
 
-  /// Minutes
-  #[arg(short, long, default_value_t = 0)]
-  minutes: u64,
+    /// Minutes
+    #[arg(short, long, default_value_t = 0)]
+    minutes: u64,
 
-  /// Seconds
-  #[arg(short, long, default_value_t = 0)]
-  seconds: u64,
+    /// Seconds
+    #[arg(short, long, default_value_t = 0)]
+    seconds: u64,
 
-  /// Print elapsed time on single line
-  #[arg(short, long)]
-  line_mode: bool,
+    /// Print elapsed time on single line
+    #[arg(short, long)]
+    line_mode: bool,
 }
 
 #[derive(Debug)]
 struct Timer {
-  duration: Arc<Mutex<Duration>>,
-  pause: Arc<AtomicBool>,
-  line_start: String,
-  line_end: String,
+    duration: Arc<Mutex<Duration>>,
+    pause: Arc<AtomicBool>,
+    line_start: &'static str,
+    line_end: &'static str,
 }
 
 impl Timer {
-  fn new(hours: u64, minutes: u64, seconds: u64, line_mode: bool) -> Self {
-    Self {
-      duration: Arc::new(Mutex::new(Duration::from_secs(seconds + minutes * 60 + hours * 3600))),
-      pause: Arc::new(AtomicBool::new(false)),
-      line_start: (if line_mode { "\r" } else { "" }).to_string(),
-      line_end: (if line_mode { "" } else { "\n" }).to_string(),
+    fn new(hours: u64, minutes: u64, seconds: u64, line_mode: bool) -> Self {
+        Self {
+            duration: Arc::new(Mutex::new(Duration::from_secs(
+                seconds + minutes * 60 + hours * 3600,
+            ))),
+            pause: Arc::new(AtomicBool::new(false)),
+            line_start: if line_mode { "\r" } else { "" },
+            line_end: if line_mode { "" } else { "\n" },
+        }
     }
-  }
 
-  fn start(&self) -> JoinHandle<Result<()>> {
-    let timer_step = Duration::from_millis(TIMER_STEP_MS);
-    let mut interval = tokio::time::interval(timer_step);
-    let duration = self.duration.clone();
-    let pause = self.pause.clone();
-    let line_start = self.line_start.clone();
-    let line_end = self.line_end.clone();
-    tokio::spawn(async move {
-      let mut stdout = tokio::io::stdout();
-      loop {
-        interval.tick().await;
-        let mut locked_duration = duration.lock().await;
-        if locked_duration.is_zero() {
-          break;
-        }
-        if !pause.load(Ordering::Relaxed) {
-          if locked_duration.subsec_millis() == 0 {
-            let time = humantime::format_duration(*locked_duration);
-            stdout
-              .write_all(format!("{line_start}{time}{line_end:>3}",).as_bytes())
-              .await?;
-            stdout.flush().await?;
-          }
+    fn start(&self) -> JoinHandle<Result<()>> {
+        let mut interval = tokio::time::interval(TIMER_STEP);
+        let duration = self.duration.clone();
+        let pause = self.pause.clone();
+        let line_start = self.line_start;
+        let line_end = self.line_end;
+        tokio::spawn(async move {
+            let mut stdout = tokio::io::stdout();
+            loop {
+                interval.tick().await;
+                let mut locked_duration = duration.lock().await;
+                if locked_duration.is_zero() {
+                    break;
+                }
+                if !pause.load(Ordering::Relaxed) {
+                    if locked_duration.subsec_millis() == 0 {
+                        let time = humantime::format_duration(*locked_duration);
+                        stdout
+                            .write_all(format!("{line_start}{time}{line_end:>3}").as_bytes())
+                            .await?;
+                        stdout.flush().await?;
+                    }
 
-          *locked_duration = locked_duration.saturating_sub(timer_step);
-        }
-      }
-      println!("{line_start}done");
-      Ok(())
-    })
-  }
+                    *locked_duration = locked_duration.saturating_sub(TIMER_STEP);
+                }
+            }
+            println!("{line_start}done");
+            Ok(())
+        })
+    }
 
-  fn toggle(&self) {
-    self.pause.store(!self.pause.load(Ordering::Relaxed), Ordering::Relaxed);
-  }
+    fn toggle(&self) {
+        self.pause
+            .store(!self.pause.load(Ordering::Acquire), Ordering::Relaxed);
+    }
 
-  async fn done(&self) -> bool {
-    self.duration.lock().await.is_zero()
-  }
+    async fn done(&self) -> bool {
+        self.duration.lock().await.is_zero()
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  let Args {
-    hours,
-    minutes,
-    seconds,
-    line_mode,
-  } = Args::parse();
-  let timer = Timer::new(hours, minutes, seconds, line_mode);
-  let timer_future = timer.start();
+    let Args {
+        hours,
+        minutes,
+        seconds,
+        line_mode,
+    } = Args::parse();
+    let timer = Timer::new(hours, minutes, seconds, line_mode);
+    let timer_future = timer.start();
 
-  let mut buf = String::new();
-  let mut stdin = BufReader::new(tokio::io::stdin());
-  while !timer.done().await {
-    match tokio::time::timeout(Duration::from_millis(TIMER_STEP_MS), stdin.read_line(&mut buf)).await {
-      Ok(result) => result?,
-      Err(_) => continue,
-    };
-    if buf == TOGGLE_CHAR.to_string() {
-      buf = String::new();
-      timer.toggle();
+    let mut stdin = tokio::io::stdin();
+    while !timer.done().await {
+        match tokio::time::timeout(TIMER_STEP, stdin.read_u8()).await {
+            Ok(Ok(TOGGLE_CHAR)) => {
+                timer.toggle();
+            }
+            _ => continue,
+        };
     }
-  }
 
-  timer_future.await??;
-  exit(0)
+    timer_future.await??;
+    std::process::exit(0)
 }
